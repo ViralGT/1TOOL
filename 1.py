7951d5bfb87362b12f32d6d691ad5cee43516bbf
ViralGT <gouravjaat93064@gmail.com>
2025-07-26 10:53:07 +0530

    once more

--- 1.py
+++ 1.py
@@ -1,1 +1,14 @@
-name =  input("what is your name")
+#repeat_name.py
+
+# Naam input lo
+name = input("what is your name : ")
+
+# Digit input lo (aur ensure karo ki wo number hi ho)
+try:
+    count = int(input("Kitni baar print karna hai?  "))
+    
+    print("\n--- Output ---")
+    for i in range(count):
+        print(f"{i+1}. {name}")
+except ValueError:
+    print("❌ Error: Sirf digit hi daal le !")
